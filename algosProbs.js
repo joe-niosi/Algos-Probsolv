9ead48f6b355416f785b1728ea9211eba7a48f6f
Joseph Niosi <niosij@gmail.com>
2023-01-18 10:48:33 -0500

    first commit 1/18

--- algosProbs.js
+++ algosProbs.js
@@ -1,3 +1,4 @@
+// 1.
 // Add two numbers
 function addNumbers(num1, num2) {
     return num1 + num2;
@@ -7,7 +8,7 @@ console.log(addNumbers(2, 5));
 console.log(addNumbers(5, 5));
 console.log(addNumbers(15, 5));
 
-
+// 2.
 // Converting minutes to seconds
 function minsToSeconds(minutes) {
     return minutes * 60;
@@ -15,3 +16,46 @@ function minsToSeconds(minutes) {
 
 console.log(minsToSeconds(1));
 console.log(minsToSeconds(5));
+
+//3.
+// Converting your age into seconds
+function ageInSeconds(age) {
+    return age * 12 * 30 * 24 * 60 * 60; // age * months * days * hours * minutes * seconds
+}
+
+console.log(ageInSeconds(25));
+
+// 4. 
+// Get the first value in an Array
+function getFirstItem(items) {
+    return items[0];
+}
+
+let movies = ['Backdraft', 'Heat', 'Moana', 'Avatar']
+
+console.log(getFirstItem(movies));
+
+// 5.
+//Label something good or bad (i.e. movies)
+
+function badOrGoodMovie(movie_rating) {
+    // badOrGoodMovie(6)-> "bad"
+    // badOrGoodMovie(7)-> "good"
+    // badOrGoodMovie(9.2)-> "good"
+    // badOrGoodMovie(2)-> "bad"
+
+    let movie_result;
+
+    if (movie_rating >= 7) {
+        movie_result = 'Good Movie'
+    } else {
+        movie_result = 'Bad Movie'
+    }
+
+    return movie_result;
+}
+
+console.log(badOrGoodMovie(5));
+console.log(badOrGoodMovie(7));
+console.log(badOrGoodMovie(9.2));
+console.log(badOrGoodMovie(2));
